fe62a491c678dd7ff08c4a077eb6039cbb3f0522
harshitshukla9752 <151209252+harshitshukla9752@users.noreply.github.com>
2024-07-05 01:57:03 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -58,7 +58,7 @@
                                 </td>
                                 <td style="padding:0px;margin:0px;">
                                     <p class="profile-title">Administrator</p>
-                                    <p class="profile-subtitle">admin@edoc.com</p>
+                                    <p class="profile-subtitle">admin@homefix.com</p>
                                 </td>
                             </tr>
                             <tr>
